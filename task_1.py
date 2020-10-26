17fbd2db1df444cec0706b0a3bd6c35e75dc90c7
daniel <daniel.szarek98@gmail.com>
2020-10-26 14:55:57 +0100

    Finish last task and better task division

--- task_1.py
+++ task_1.py
@@ -1,69 +1,75 @@
 import numpy as np
 
-
-def insert_brake():
-    print("========================================================")
+counter = 1
 
 
+def mark_new_task():
+    global counter
+    print(f"TASK {counter}. ========================================================")
+    counter += 1
+
+
+mark_new_task()
 zeroes = np.zeros(10)
-print(f"1. {zeroes}")
-insert_brake()
+print(zeroes)
 
+mark_new_task()
 fifths = np.full(10, 5)
-print(f"2. {fifths}")
-insert_brake()
+print(fifths)
 
+
+mark_new_task()
 from_10_to_50 = np.arange(10, 50, 1)
-print(f"3. {from_10_to_50}")
-insert_brake()
+print(from_10_to_50)
 
+mark_new_task()
 matrix = np.arange(9).reshape(3, 3)
-print(f"4. {matrix}")
-insert_brake()
+print(matrix)
 
+mark_new_task()
 identity_matrix = np.eye(3)
-print(f"5. {identity_matrix}")
-insert_brake()
+print(identity_matrix)
 
+mark_new_task()
 normal_distribution_matrix = np.random.normal(size=(5, 5))
-print(f"6. {normal_distribution_matrix}")
-insert_brake()
+print(normal_distribution_matrix)
 
+mark_new_task()
 matrix2 = np.arange(0.01, 1.01, 0.01).reshape(10, 10)
-print(f"7. {matrix2}")
-insert_brake()
+print(matrix2)
 
+mark_new_task()
 linspace = np.linspace(0, 1, 20)
-print(f"8. {linspace}")
-insert_brake()
+print(linspace)
 
+mark_new_task()
 rand = np.random.randint(low=1, high=25, size=25).reshape(5, 5)
-print(f"9. {rand}")
+print(rand)
 print(rand.sum())
 print(np.average(rand))
 print(np.std(rand))
 print(rand.sum(axis=0))
-insert_brake()
 
+mark_new_task()
 rand2 = np.random.randint(low=1, high=100, size=(5, 5))
-print(f"10. {rand2}")
+print(rand2)
 print(np.median(rand2))
 print(rand2.min())
 print(rand2.max())
-insert_brake()
 
+mark_new_task()
 rand2 = np.random.randint(low=1, high=100, size=(5, 7))
-print(f"11. {rand2.T}")
-insert_brake()
+print(rand2.T)
 
+mark_new_task()
 matrix_a = np.random.randint(low=1, high=100, size=(4, 7))
 matrix_b = np.random.randint(low=1, high=100, size=(4, 7))
-print(f"12. {np.add(matrix_a, matrix_b)}")
-insert_brake()
+print(np.add(matrix_a, matrix_b))
 
+mark_new_task()
 matrix_a = np.random.randint(low=1, high=100, size=(4, 5))
 matrix_b = np.random.randint(low=1, high=100, size=(5, 4))
-# print(np.multiply(matrix_a, matrix_b))
-print(f"13. {np.matmul(matrix_a, matrix_b)}")
+print(np.matmul(matrix_a, matrix_b))
+print(np.dot(matrix_a, matrix_b))
 
 
