b11a108d9101ba0d1a7a9316fdf7cfcd62f1ac27
daniel <daniel.szarek98@gmail.com>
2020-10-29 20:08:15 +0100

    Finished task 4

--- task-4/task_4.py
+++ task-4/task_4.py
@@ -1,5 +1,8 @@
+import string
 import requests
 
+from re import compile
+from random import choices
 from openpyxl import Workbook
 from bs4 import BeautifulSoup
 
@@ -12,8 +15,42 @@ class WebScrapping:
         self.ws_links = self.wb.create_sheet("Linki")
         self.ws_filmweb = self.wb.create_sheet("Filmweb")
 
+    @staticmethod
+    def exchange_get_code():
+        return "".join(choices(string.ascii_lowercase, k=3))
+
+    @staticmethod
+    def exchange_get_url(code):
+        return f"https://stooq.pl/q/?s={code}"
+
+    def exchange_parse(self, htmls_to_parse):
+        self.ws_exchange.append([
+            'Kod',
+            'Kurs',
+            'Zmiana',
+            'Ilość transakcji'
+        ])
+        for code, html in htmls_to_parse.items():
+            soup = BeautifulSoup(html, 'html.parser')
+            price = soup.find("span", id=compile(f"aq_{code}_c\d")).text
+            change = soup.find("span", id=f"aq_{code}_m3").text.strip("()")
+            transactions_amount = soup.find("span", id=f"aq_{code}_n1").text
+            self.ws_exchange.append([
+                code,
+                price,
+                change,
+                transactions_amount
+            ])
+
     def exchange_task(self):
-        pass
+        htmls_to_parse = {}
+        while len(htmls_to_parse) < 5:
+            code = self.exchange_get_code()
+            url = self.exchange_get_url(code)
+            response = requests.get(url)
+            if response.status_code == 200 and "nie istnieje w bazie" not in response.text:
+                htmls_to_parse[code] = response.text
+        self.exchange_parse(htmls_to_parse)
 
     def links_task(self):
         response = requests.get("https://djangopackages.org/")
@@ -46,11 +83,13 @@ class WebScrapping:
             rating
         ])
 
-    def save_document(self, filename='document.xlsx'):
+    def save_document(self, filename='Szarek-175IC_A2.xlsx'):
         self.wb.save(filename)
 
 
-ws = WebScrapping()
-ws.links_task()
-ws.filmweb_task()
-ws.save_document()
+if __name__ == "__main__":
+    ws = WebScrapping()
+    ws.exchange_task()
+    ws.links_task()
+    ws.filmweb_task()
+    ws.save_document()
