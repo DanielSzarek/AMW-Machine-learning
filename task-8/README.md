9b6c26d1ba54f56f9e6bfe6a0183bd224bbd1388
daniel <daniel.szarek98@gmail.com>
2020-12-11 15:22:06 +0100

    Task 8 - CSV and readme

--- task-8/README.md
+++ task-8/README.md
@@ -1,1 +1,45 @@
 # Readme
+
+## JSON
+*_json.dump()_* - serializacja obiektu wskazanego w pierwszym argumencie (obj) do JSON i zapis do pliku - drugi argument (fp)
+
+*_json.dumps()_* - serializacja obiektu wskazanego w pierwszym argumencie (obj) do JSON. Możliwość określenia ilości spacji wciecia poprzez: _indent_.
+
+*_json.load()_* - deserializacja JSONa z pliku na odpowiedni obiekt Python.
+
+*_json.loads()_* - deserializacja JSONa ze stringa na odpowiedni obiekt Python. Mamy możliwosc utworzenia wlasnie deserializatora.
+
+*_json.JSONEncoder_* - przy pomocy tej klasy możemy utworzyć własną klasę po niej dziedziczącą, która umożliwi implementacje serializacji dla specyficznego typu
+
+## JSON - pomocnicze metody
+
+*_requests.get()_* - wysyłka żadania HTTP GET
+
+*_sorted()_* - funckja zwraca nową posortowaną listę, możemy określić szczegółowo sortowanie
+
+*_join()_* - ma wiele zastosować. Zarówno łączy pojedyczne stringi, jak i te zawarte w listach. Poza tym ułatwia tworzenie ścieżek i URL.
+
+*_filter()_* - filtruje przekazaną listę według zadanej funkcji
+
+*_isinstance()_* - funkcja sprawdza, czy obiekt jest zadanego typu. Zwraca true/false.
+
+## CSV
+
+*_csv.reader()_* - odczytuje każdy obiekt, który iteruje linia po linii. Zwraca obiekt z możliwości iteracji po nim. Możemy wskazać co oddziela kolejne wartości np. przecinek w CSV.
+
+*_csv.DictReader()_* - zwraca obiekt, który przekształcił linie pliku CSV w słownik (dict).
+
+*_csv.writer()_* - funkcja zwraca obiekt, który pomoże w zapisie obiektu do pliku CSV. Możemy wskazać, co będzie oddzielało wartości w pliku.
+
+*_csv.DictWriter()_* - funkcja zwraca obiekt, który pomoże w zapisie obiektu do pliku CSV ze słowników.
+
+## CSV i Pandas
+
+*_pandas.read_csv()_* - funkcja odczytuje plik CSV i przekstałca go od razu na obiekt DataFrame. Daje dużo możliwości, np. określenie, która kolumna jest indeksem.
+
+*_DataFrame.to_csv()_* - funkcja umożliwia zapisu obiektu DataFrame do pliku CSV.
+
+
+
+
+
--- task-8/task_8_csv.py
+++ task-8/task_8_csv.py
@@ -1,5 +1,16 @@
 import csv
+import pandas as pd
 
+counter = 1
+
+
+def new_task():
+    global counter
+    print(f"\n{counter}. ========================================================")
+    counter += 1
+
+
+new_task()
 with open('employee_birthday.txt') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
@@ -11,6 +22,7 @@ with open('employee_birthday.txt') as csv_file:
         line_count += 1
     print(f"Processed {line_count} lines")
 
+new_task()
 with open('employee_birthday.txt', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     line_count = 0
@@ -22,5 +34,43 @@ with open('employee_birthday.txt', mode='r') as csv_file:
         line_count += 1
     print(f"Processed {line_count} lines")
 
+new_task()
+with open('sports.csv', mode='w') as sport_file:
+    sport_writer = csv.writer(sport_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
+    sport_writer.writerow(['tennis', 1])
+    sport_writer.writerow(['football', 11])
+    sport_writer.writerow(['box', '1'])
 
+new_task()
+with open('sports2.csv', mode='w') as sport2_file:
+    fieldnames = ['sport_name', 'players']
+    sport_writer = csv.DictWriter(sport2_file, fieldnames=fieldnames)
+
+    sport_writer.writeheader()
+    sport_writer.writerow({'sport_name': 'basketball', 'players': 5})
+    sport_writer.writerow({'sport_name': 'football', 'players': 11})
+
+new_task()
+data_frame = pd.read_csv('hrdata.csv')
+print(data_frame)
+
+data_frame_index = pd.read_csv('hrdata.csv', index_col='Name')
+print(data_frame_index)
+
+df = pd.read_csv('hrdata.csv', index_col='Name', parse_dates=['Hire Date'])
+print(df)
+
+df = pd.read_csv('hrdata.csv',
+                 index_col='Employee',
+                 parse_dates=['Hired'],
+                 header=0,
+                 names=['Employee', 'Hired', 'Salary', 'Sick Days'])
+print(df)
+
+df = pd.read_csv('hrdata.csv',
+                 index_col='Employee',
+                 parse_dates=['Hired'],
+                 header=0,
+                 names=['Employee', 'Hired', 'Salary', 'Sick Days'])
+df.to_csv('hrdata_modified.csv')
